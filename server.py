c47aabdb6a31c7443f7c76925f552bc38b4d1b54
Michal Jackowski <michal.jackowski1@gmail.com>
2018-06-13 21:55:24 +0200

    Server implemantation

--- server.py
+++ server.py
@@ -1,13 +1,20 @@
 from decimal import Decimal, getcontext
+from flask import Flask
 
 
 def calculate_pi_Bailey_Borwein_Plouffe_formula(start, end):
     getcontext().prec=6000
-    return (sum(1/Decimal(16)**k *
+    return str(sum(1/Decimal(16)**k *
                 (Decimal(4)/(8*k+1) -
                  Decimal(2)/(8*k+4) -
                  Decimal(1)/(8*k+5) -
                  Decimal(1)/(8*k+6)) for k in range(start, end)))
 
 
-print(calculate_pi_Bailey_Borwein_Plouffe_formula(0, 4000))
+app = Flask(__name__)
+
+@app.route('/<id>')
+def hello_world(id):
+    return calculate_pi_Bailey_Borwein_Plouffe_formula(0, 4000) + ' id = ' + str(id)
+
+
